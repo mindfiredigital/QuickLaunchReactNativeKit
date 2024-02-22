25f5c09d2e05af36f15b4a889c618804ecdc4b80
Madhav Nasit <madhavn@mindfiresolutions.com>
2024-02-22 20:37:10 +0530

    Add RESET_STATE action to reset redux store

--- template/src/store/reducets.ts
+++ template/src/store/reducets.ts
@@ -1,15 +1,39 @@
-import {combineSlices} from '@reduxjs/toolkit';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import authReducer from './slices/authSlice';
 import {persistReducer} from 'redux-persist';
+import {UnknownAction, combineSlices} from '@reduxjs/toolkit';
+import authReducer from './slices/authSlice';
 
-// persistence config for auth reducer
+export const RESET_STATE = 'RESET_STATE';
+
+/**
+ * persistence config for auth reducer
+ */
 const persistAuthConfig = {
   key: 'auth',
   storage: AsyncStorage,
   whitelist: ['isAuthenticated'],
 };
 
-export const rootReducer = combineSlices({
+/**
+ * Combine all the reducers
+ */
+const appReducer = combineSlices({
   auth: persistReducer(persistAuthConfig, authReducer),
 });
+
+/**
+ * Return root reduces
+ * Provides method to reset redux state
+ * @param state
+ * @param action
+ */
+export const rootReducer = (state: any, action: UnknownAction) => {
+  // Reset redux state
+  if (action.type === RESET_STATE) {
+    AsyncStorage.removeItem('persist:root');
+    AsyncStorage.removeItem('persist:auth');
+    state = undefined;
+  }
+
+  return appReducer(state, action);
+};
