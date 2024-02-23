bba3862a4e7c4679e9f865718f8cfae2297e892a
Pratul dawande <>
2024-02-23 16:20:30 +0530

    add font sizes in style sheets

--- template/src/components/Text.tsx
+++ template/src/components/Text.tsx
@@ -1,22 +1,30 @@
-import React from "react"
-import { StyleProp, Text as RNText, TextProps as RNTextProps, TextStyle, StyleSheet } from "react-native"
-import { useTheme } from "@react-navigation/native"
-import { Colors, typography } from "../theme"
-
-
+import React from 'react';
+import {
+  StyleProp,
+  Text as RNText,
+  TextProps as RNTextProps,
+  TextStyle,
+  StyleSheet,
+} from 'react-native';
+import {useTheme} from '@react-navigation/native';
+import {Colors, typography} from '../theme';
 export interface TextProps extends RNTextProps {
   /**
    * The text to display if not using `tx` or nested components.
    */
-  text?: string
+  text?: string;
   /**
    * An optional style override useful for padding & margin.
    */
-  style?: StyleProp<TextStyle>
+  style?: StyleProp<TextStyle>;
+  /**
+   * Text size modifier.
+   */
+  size?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'body1' | 'body2' | 'body3';
   /**
    * Children components.
    */
-  children?: React.ReactNode
+  children?: React.ReactNode;
 }
 
 /**
@@ -26,28 +34,64 @@ export interface TextProps extends RNTextProps {
  * @returns {JSX.Element} The rendered `Text` component.
  */
 export function Text(props: TextProps): JSX.Element {
-  const {  text, children, style: styleOverride, ...rest } = props
+  const {text, children, style: styleOverride, size = 'h1', ...rest} = props;
   const {colors} = useTheme();
   const styles = makeStyles(colors);
 
-  const content =  text || children
+  const content = text || children;
   const $styles: StyleProp<TextStyle> = [
     styles.text,
+    styles[size],
     styleOverride,
-  ]
+  ];
 
   return (
     <RNText {...rest} style={$styles}>
       {content}
     </RNText>
-  )
+  );
 }
 
 const makeStyles = (colors: Colors) =>
   StyleSheet.create({
     text: {
-      fontSize: 20,
       color: colors.text,
       fontFamily: typography.regular,
+    },
+    h1: {
+      fontSize: 30,
+      lineHeight: 34,
+    },
+    h2: {
+      fontSize: 24,
+      lineHeight: 30,
+    },
+    h3: {
+      fontSize: 18,
+      lineHeight: 22,
+    },
+    h4: {
+      fontSize: 15,
+      lineHeight: 18,
+    },
+    h5: {
+      fontSize: 12,
+      lineHeight: 14,
+    },
+    h6: {
+      fontSize: 10,
+      lineHeight: 12,
+    },
+    body1: {
+      fontSize: 15,
+      lineHeight: 28,
+    },
+    body2: {
+      fontSize: 14,
+      lineHeight: 18,
+    },
+    body3: {
+      fontSize: 16,
+      lineHeight: 18,
     },
   });
