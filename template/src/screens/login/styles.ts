35ceef87f952fdb838cc4344d192f788c2a7cf81
Madhav Nasit <madhavn@mindfiresolutions.com>
2024-02-27 18:55:21 +0530

    Refactor login screen and it's styles

---
+++ template/src/screens/login/styles.ts
@@ -0,0 +1,33 @@
+import {StyleSheet, ViewStyle} from 'react-native';
+import {Colors, spacing} from '../../theme';
+import {s, vs} from '../../utils';
+
+const makeStyles = (colors: Colors) =>
+  StyleSheet.create({
+    contentContainerStyle: {
+      flexGrow: 1,
+      justifyContent: 'space-between',
+    } as ViewStyle,
+    headerWrapper: {
+      paddingHorizontal: s(spacing.md),
+      marginBottom: vs(spacing.lg),
+    } as ViewStyle,
+    logoContainer: {
+      marginTop: vs(spacing.md),
+      marginBottom: vs(spacing.lg),
+      alignSelf: 'center',
+    } as ViewStyle,
+    forgotView: {
+      paddingHorizontal: 0,
+      marginHorizontal: s(spacing.md),
+      marginBottom: vs(spacing.lg),
+      alignSelf: 'flex-end',
+    },
+    bottomView: {
+      flexDirection: 'row',
+      justifyContent: 'center',
+      marginBottom: vs(spacing.xs),
+    },
+  });
+
+export default makeStyles;
