3983dbe32a049fc8e65b46daf12b5e5784f24ed0
Madhav Nasit <madhavn@mindfiresolutions.com>
2024-03-15 17:28:31 +0530

    remove: Deleted png icons replaced by svg

--- QuickLaunchReactNativeKit/src/assets/icons/index.ts
+++ QuickLaunchReactNativeKit/src/assets/icons/index.ts
@@ -1,15 +1,3 @@
 export const iconRegistry = {
-  hidden: require('./hidden.png'),
-  view: require('./view.png'),
-  mindfireFireLogo: require('./mindfireFireLogo.png'),
-  lock: require('./lock.png'),
-  people: require('./people.png'),
-  back: require('./back.png'),
-  email: require('./email.png'),
-  googleLight: require('./googleLight.png'),
-  googleDark: require('./googleDark.png'),
-  faceRecognition: require('./face-recognition.png'),
-  fingerprint: require('./fingerprint.png'),
-  appleLight: require('./appleLight.png'),
-  appleDark: require('./appleDark.png'),
+  logo: require('./mindfireFireLogo.png'),
 };
